f990a761dfb314c23b849031593a51e57343b205
EdwardMelendezM <192666@unsaac.edu.pe>
2023-04-27 07:39:39 -0500

    adding onPause, onError, loop, light, onStart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import ReactPlayer from "react-player";
 import { useState, useRef } from 'react'
-
+import "./App.css"
 
 
 const App = () => {
@@ -10,26 +10,42 @@ const App = () => {
     console.log(ref.current);
 
     if (!isAuth) {
-      alert("Dont have authorization")
       if (ref.current) {
-
-        console.log(ref.current.player);
-
+        console.log(ref.current);
       }
       return;
     }
   }
+  const handleClickOnPause = () => {
+    console.log("Esto se pauseo");
+
+  }
+  const handleClickOnError = () => {
+    console.log("Ocurrio un error");
+
+  }
+  const handleOnStart = () => {
+    setTimeout(() => {
+      console.log("Esperando");
+
+    }, 2000);
+    if (!ref.current) return
+    ref.current.playing = true
+  }
   return (
-    <div>
+    <div className="container">
       <ReactPlayer
         url='/video.mp4'
         controls
         muted
+        onPause={handleClickOnPause}
         onPlay={handleClickOnPlay}
         ref={ref}
-
+        loop
+        onError={handleClickOnError}
+        light={true}
+        onStart={handleOnStart}
       />
-      <button onClick={() => setIsAuth(true)}>Auth</button>
     </div>
   );
 }
